ab0f01928093a4e59b18f07b62ec8723dd276f43
Maxim Skryabin <mxmskrbn@gmail.com>
2022-01-29 17:30:01 +0300

    Created example project

---
+++ Example/devcred-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  devcred-example
+//
+//  Created by Maxim Skryabin on 29.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
